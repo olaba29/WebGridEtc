c454ac34cc6d4d7964754a2a91e498ab46cb0261
olaba29 <andoni.ola@gmail.com>
2021-12-07 20:49:51 +0100

    Progress

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function alerta(){
-    var izena = document.getElementById('izena').value;
-    alert("Kaixo " + izena);
+    var k1 = 0;
+    var k2 = 0;
+
+    var izena = document.getElementById('izena').value; /*Izena id-aren bidez*/
+    if (/^Andoni/.test(izena)) { /* / / artean expresioa */
+        k1 = 1;
+    }
+
+    var abizena = document.getElementById('abizena').value;
+    if (/^Olabarria/.test(abizena)) {
+        k2 = 1;
+    }
+
+    if (k1+k2!=2) {
+        alert("Kaixo " + izena + " " + abizena + ", ez zara Andoni Olabarria");
+    } else {
+        alert("Ongi etorri jefe");
+    }
 }
